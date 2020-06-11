9cb21ef5ec3bdef308814e5fe7e4bf89dbabd8e2
duanqimiao <duanqimiao@gmail.com>
2020-06-11 16:51:15 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件 
+# 生成静态文件
 npm run build
 
 # 进入生成的文件夹
@@ -26,7 +26,14 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
 
 # deploy to coding
-# echo 'www.duanqimiao.com\nduanqimiao.com' > CNAME  # 自定义域名
+# 如果是发布到自定义域名
+# echo 'xugaoyi.com' > CNAME
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:<duanqimiao>/<duanqimiao>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
   codingUrl=git@e.coding.net:xgy/xgy.git
 else
