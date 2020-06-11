a7fc53691ff088972f5051b47b5c1ec14b9782b0
duanqimiao <duanqimiao@gmail.com>
2020-06-11 17:24:22 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # echo 'xugaoyi.com' > CNAME
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<duanqimiao>/<duanqimiao>.github.io.git master
+git push -f git@github.com:duanqimiao/duanqimiao.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
